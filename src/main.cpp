3c61fdd1bf96ed820182c3c07b73be57fe2adb86
alaa alassi <alaa.alassi@nobleo.nl>
2022-01-12 00:06:05 +0100

    set aspect ratio

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
-int main() {
-    std::cout << "Hello World!" << "\n";
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+
+    //create a gui window:
+    namedWindow("Output", 1);
+
+    //initialize a 120X350 matrix of black pixels:
+    double aspectRatio =0.9;
+    double windowWidth = int(aspectRatio*1920);
+    int windowLength = int(aspectRatio*1080);
+
+    Mat output = Mat::zeros(windowLength,windowWidth , CV_8UC3);
+
+int thickness = 1;
+int shift = 0;
+    rectangle(output,
+              Point(15, 70),
+              Point(2000, 2000),
+              Scalar(0, 255, 0),
+              thickness = 1,
+              LINE_8,
+              shift = 0);
+
+        //display the image:
+        imshow("Output", output);
+
+    //wait for the user to press any key:
+    waitKey(0);
+
     return 0;
 }
