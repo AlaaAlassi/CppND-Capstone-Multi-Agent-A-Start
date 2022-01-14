2c16861a5a9d402c4a315365643e836bd6a3b4a0
alaa alassi <alaa.alassi@nobleo.nl>
2022-01-14 23:11:54 +0100

    refactor map

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "CellData.hpp"
+#include "Warehouse.hpp"
 #include <vector>
+
 
 using namespace cv;
 
@@ -11,39 +13,7 @@ const int MAX_MONITOR_WIDTH = 1920;
 int main(int argc, char **argv)
 {
     //construct a grid
-    std::vector<CellData> map;
-    double cellSize = 35; //meters/cell
-    int xSize = 35;
-    int ySize = 21;
-    for (int j = 0; j < ySize; j++)
-    {
-        for (int i = 0; i < xSize; i++)
-        {
-            int index = i + xSize * (j);
-            map.emplace_back(j, i, index, cellSize);
-        }
-    }
-
-    for(auto &cell:map){
-        if (cell.rowIndex == 2 || cell.rowIndex == 6 || cell.rowIndex == 10 || cell.rowIndex == 14 || cell.rowIndex == 18){
-            cell.value = CellValue::occupied;
-        }
-        if (cell.columnsIndex == 1 || cell.columnsIndex == 2 || cell.columnsIndex == 4 || cell.columnsIndex == 5 ||
-            cell.columnsIndex == 29 || cell.columnsIndex == 30 || cell.columnsIndex == 32 || cell.columnsIndex == 33){
-            cell.value = CellValue::delivary;
-        }
-        if (cell.rowIndex == 0 || cell.rowIndex == 20){
-            cell.value = CellValue::emptey;
-        }
-        if (cell.rowIndex%2 > 0){
-            cell.value = CellValue::delivary;
-        }
-        if (cell.columnsIndex == 0 || cell.columnsIndex == 3 || cell.columnsIndex == 6 || cell.columnsIndex == 17 ||
-        cell.columnsIndex ==  27 || cell.columnsIndex ==  28 || cell.columnsIndex == 31 || cell.columnsIndex == 34){
-            cell.value = CellValue::emptey;
-        }
-
-    }
+    std::vector<CellData> map = Warehouse::getDefultMap();
 
     //create a gui window:
     namedWindow("Output", 1);
