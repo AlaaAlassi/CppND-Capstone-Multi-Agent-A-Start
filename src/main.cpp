9e01aeebc1aeeb27c578f9501801c5dc206cc613
alaa alassi <alaa.alassi@nobleo.nl>
2022-01-19 23:31:07 +0100

    example of multi robots sync moving after all goals reached

--- src/main.cpp
+++ src/main.cpp
@@ -18,22 +18,27 @@ int main(int argc, char **argv)
 {
     //construct a grid
     std::vector<CellData> map = Warehouse::getDefultMap();
-
     double aspectRatio = 0.7;
     int windowWidth = int(aspectRatio * MAX_MONITOR_WIDTH);
     int windowLength = int(aspectRatio * MAX_MONITOR_LENGTH);
-    auto rob1 = std::make_shared<Robot>(map.at(0).cartesianPosition);
     Graphics A = Graphics(windowLength, windowWidth, map);
+    auto rob1 = std::make_shared<Robot>(map.at(0).cartesianPosition);
+    auto rob2 = std::make_shared<Robot>(map.at(0).cartesianPosition);
     A._robots.push_back(rob1);
+    A._robots.push_back(rob2);
     A.loadBackgroundImg();
     std::thread simulationThread(&Graphics::simulate, &A);
     Cartesian2DPoint goal;
+    Cartesian2DPoint goal2;
     for (auto const &cell : map)
     {
         goal = cell.cartesianPosition;
+        goal2 = cell.cartesianPosition;
         double stepDistance = 1;
         std::future<bool> ftr = std::async(std::launch::async, &Robot::trackGoalPosition,rob1, goal, stepDistance);
+        std::future<bool> ftr2 = std::async(std::launch::async, &Robot::trackGoalPosition,rob2, goal2, 0.5);
         ftr.get();
+        ftr2.get();
     }
     std::cout << "Goal reached, distance error: " << rob1->distanceToPoint(goal) << std::endl;
     //wait for the user to press any key:
